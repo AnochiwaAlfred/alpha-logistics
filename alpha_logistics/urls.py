28e0d244592936cd108e1b2cfaafab45ae827a50
AnochiwaAlfred <anochiwaalfred@gmail.com>
2023-07-18 18:19:32 +0100

    Updatedddef

--- alpha_logistics/urls.py
+++ alpha_logistics/urls.py
@@ -1,11 +1,18 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.views.static import serve
+from django.conf import settings
+from django.urls import path, include, re_path
 from apis.api import api
+
+
 VERSION = 'v1'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(f"api/{VERSION}/", api.urls )
+    path(f"api/{VERSION}/", api.urls ),
+    
+    re_path(r'^media/(?P<path>.*)$', serve,  {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]
