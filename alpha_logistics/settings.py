1dbdbd975794b25b68befc5872be9c3206893f3f
AnochiwaAlfred <anochiwaalfred@gmail.com>
2023-07-18 17:40:10 +0100

    Update

--- alpha_logistics/settings.py
+++ alpha_logistics/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
