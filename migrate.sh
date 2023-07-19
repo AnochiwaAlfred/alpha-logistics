7ea00a860a77037d8e6d44e07b7f82a4df2e8174
Anochiwa Alfred <42777193+AnochiwaAlfred@users.noreply.github.com>
2023-07-19 08:21:11 +0100

    Delete migrate.sh

--- migrate.sh
+++
@@ -1,9 +0,0 @@
-python3 manage.py migrate authuser
-python3 manage.py migrate categories
-python3 manage.py migrate dispatch
-python3 manage.py migrate orders
-python3 manage.py migrate payments
-python3 manage.py migrate products
-python3 manage.py migrate sub_categories
-python3 manage.py migrate tasks
-python3 manage.py migrate
